5fe1e3e5d483012e1426ba4328116bbc8a521ad3
AlexKhorosh <alexandrkhorosh@mail.ru>
2025-04-14 17:01:23 +0300

    first commit ads

---
+++ dom/ads/task.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const rotators = document.querySelectorAll('.rotator');
+  
+    rotators.forEach(rotator => {
+      const cases = rotator.querySelectorAll('.rotator__case');
+      let activeIndex = 0;
+  
+      setInterval(() => {
+        cases[activeIndex].classList.remove('rotator__case_active');
+        activeIndex = (activeIndex + 1) % cases.length; 
+        cases[activeIndex].classList.add('rotator__case_active');
+  
+        const speed = parseInt(cases[activeIndex].getAttribute('data-speed'), 10);
+        clearInterval(intervalId);
+        intervalId = setInterval(() => {
+          cases[activeIndex].classList.remove('rotator__case_active');
+          activeIndex = (activeIndex + 1) % cases.length;
+          cases[activeIndex].classList.add('rotator__case_active');
+        }, speed);
+      }, 1000);
+    });
+  });
+  
